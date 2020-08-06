7372355c8f211283a56b8f0cb9b564ca06b764c0
fstomazini <felipestomazini@gmail.com>
2020-08-06 17:17:24 -0300

    Adicionados SellerDao e DepartmentDao como interfaces

---
+++ src/model/dao/DepartmentDao.java
@@ -0,0 +1,16 @@
+package model.dao;
+
+import model.entities.Department;
+
+import java.util.List;
+
+public interface DepartmentDao {
+
+    void insert(Department obj);
+    void update(Department obj);
+    Department deleteById(Integer id);
+    Department findById(Integer id);
+    List<Department> findAll();
+
+
+}
