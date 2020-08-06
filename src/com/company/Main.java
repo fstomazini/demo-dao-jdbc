b0cd0d9bf42d362768e24e374b3f16b0a23e8532
fstomazini <felipestomazini@gmail.com>
2020-08-06 16:28:52 -0300

    adicionada classe Department

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import model.entities.Department;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Department obj = new Department(1,"Devolop");
+        System.out.println(obj);
     }
 }
